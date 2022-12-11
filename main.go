6c52882f4c774b1d722a4f16e023306f9a07cc97
I Made Sariyanta <desar@sariyanta.com>
2022-12-11 22:48:53 +0100

    Error checking

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -24,6 +25,26 @@ func About(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the about page. Sum is %d", sum)
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	sum, err := divideValue(10.0, 1.0)
+
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err)
+		return
+	}
+
+	fmt.Fprintf(w, "Sum is %f", sum)
+}
+
 func addValue(x, y int) int {
 	return x + y
+}
+
+func divideValue(x, y float32) (float32, error) {
+
+	if y <= 0 {
+		return 0, errors.New("cannot divide by zero")
+	}
+	result := x / y
+	return result, nil
 }
