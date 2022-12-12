638e944a307a42817e2e6b1e14cfb8aa4fc2a9a2
I Made Sariyanta <desar@sariyanta.com>
2022-12-12 09:06:17 +0100

    Serving HTML templates

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -17,34 +17,19 @@ func main() {
 }
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page")
 }
 
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addValue(2, 3)
-	fmt.Fprintf(w, "This is the about page. Sum is %d", sum)
+	renderTemplate(w, "about.page")
 }
 
-func Divide(w http.ResponseWriter, r *http.Request) {
-	sum, err := divideValue(10.0, 1.0)
-
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parseTemplate, _ := template.ParseFiles("./templates/" + tmpl + ".tmpl")
+	err := parseTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "Error: %s", err)
+		fmt.Println("error parsing template:", err)
 		return
 	}
 
-	fmt.Fprintf(w, "Sum is %f", sum)
-}
-
-func addValue(x, y int) int {
-	return x + y
-}
-
-func divideValue(x, y float32) (float32, error) {
-
-	if y <= 0 {
-		return 0, errors.New("cannot divide by zero")
-	}
-	result := x / y
-	return result, nil
 }
