85f271aee4cfeb2fdbd777308c98daaf92b542b9
I Made Sariyanta <desar@sariyanta.com>
2022-12-12 10:14:26 +0100

    Serve static folder

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -13,6 +13,9 @@ func main() {
 	http.HandleFunc("/", handlers.Home)
 	http.HandleFunc("/about", handlers.About)
 
+	fs := http.FileServer(http.Dir("./static/"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
 	fmt.Println("Server is running on port", PORT)
 	_ = http.ListenAndServe(PORT, nil)
 }
